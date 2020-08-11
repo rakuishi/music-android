6076d9b44b32cda3bff095189c61c4015cddece8
rakuishi <rakuishi@gmail.com>
2020-08-12 07:32:36 +0900

    First commit

---
+++ app/src/main/java/com/rakuishi/music/MainActivity.kt
@@ -0,0 +1,18 @@
+package com.rakuishi.music
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.rakuishi.music.ui.main.MainFragment
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.main_activity)
+        if (savedInstanceState == null) {
+            supportFragmentManager.beginTransaction()
+                    .replace(R.id.container, MainFragment.newInstance())
+                    .commitNow()
+        }
+    }
+}
