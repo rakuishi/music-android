28a36d9c7ec32df6f00e8d6af0dc673ddd302515
rakuishi <rakuishi@gmail.com>
2020-08-15 13:49:56 +0900

    Support audio focus and audio output

--- app/src/main/java/com/rakuishi/music/presentation/music/MusicPlayerService.kt
+++ app/src/main/java/com/rakuishi/music/presentation/music/MusicPlayerService.kt
@@ -1,13 +1,22 @@
 package com.rakuishi.music.presentation.music
 
+import android.content.BroadcastReceiver
+import android.content.Context
+import android.content.Intent
+import android.content.IntentFilter
+import android.media.AudioManager
 import android.os.Bundle
 import android.support.v4.media.MediaBrowserCompat
 import android.support.v4.media.MediaMetadataCompat
 import android.support.v4.media.session.MediaSessionCompat
 import android.support.v4.media.session.PlaybackStateCompat
+import androidx.media.AudioAttributesCompat
+import androidx.media.AudioFocusRequestCompat
+import androidx.media.AudioManagerCompat
 import androidx.media.MediaBrowserServiceCompat
 import com.rakuishi.music.data.MusicRepository
 import dagger.hilt.android.AndroidEntryPoint
+import timber.log.Timber
 import javax.inject.Inject
 
 // https://developer.android.com/guide/topics/media-apps/audio-app/building-a-mediabrowserservice
@@ -27,6 +36,19 @@ class MusicPlayerService : MediaBrowserServiceCompat(), MusicPlayer.Callback {
     private lateinit var mediaSession: MediaSessionCompat
     private lateinit var stateBuilder: PlaybackStateCompat.Builder
     private lateinit var notificationManager: MusicNotificationManager
+
+    // https://developer.android.com/guide/topics/media-apps/audio-focus
+    private lateinit var audioFocusRequest: AudioFocusRequestCompat
+    private lateinit var audioManager: AudioManager
+
+    // https://developer.android.com/guide/topics/media-apps/volume-and-earphones
+    private val audioNoisyFilter = IntentFilter(AudioManager.ACTION_AUDIO_BECOMING_NOISY)
+    private val audioNoisyReceiver = object : BroadcastReceiver() {
+        override fun onReceive(context: Context, intent: Intent) {
+            mediaSession.controller.transportControls.pause()
+        }
+    }
+
     private var metadataList: List<MediaMetadataCompat> = arrayListOf()
     private var albumId: Long = -1
 
@@ -50,6 +72,29 @@ class MusicPlayerService : MediaBrowserServiceCompat(), MusicPlayer.Callback {
 
         notificationManager =
             MusicNotificationManager(this, mediaSession)
+
+        // Audio focus
+        audioFocusRequest = AudioFocusRequestCompat.Builder(AudioManagerCompat.AUDIOFOCUS_GAIN)
+            .setAudioAttributes(
+                AudioAttributesCompat.Builder()
+                    .setUsage(AudioAttributesCompat.USAGE_MEDIA)
+                    .setContentType(AudioAttributesCompat.CONTENT_TYPE_MUSIC)
+                    .build()
+            )
+            .setOnAudioFocusChangeListener { focusChange ->
+                when (focusChange) {
+                    AudioManager.AUDIOFOCUS_GAIN -> {
+                        mediaSession.controller.transportControls.play()
+                    }
+                    AudioManager.AUDIOFOCUS_LOSS_TRANSIENT,
+                    AudioManager.AUDIOFOCUS_LOSS -> {
+                        mediaSession.controller.transportControls.pause()
+                    }
+                }
+            }
+            .build()
+
+        audioManager = getSystemService(AudioManager::class.java)
     }
 
     override fun onDestroy() {
@@ -107,12 +152,16 @@ class MusicPlayerService : MediaBrowserServiceCompat(), MusicPlayer.Callback {
         }
 
         override fun onPlay() {
-            mediaSession.isActive = true
-            mediaSession.setMetadata(metadataList[musicPlayer.getCurrentWindowIndex()])
-            musicPlayer.play()
-            setNewState(PlaybackStateCompat.STATE_PLAYING)
+            if (canGainAudioFocus()) {
+                registerReceiver(audioNoisyReceiver, audioNoisyFilter)
 
-            notificationManager.start()
+                mediaSession.isActive = true
+                mediaSession.setMetadata(metadataList[musicPlayer.getCurrentWindowIndex()])
+                musicPlayer.play()
+                setNewState(PlaybackStateCompat.STATE_PLAYING)
+
+                notificationManager.start()
+            }
         }
 
         override fun onPause() {
@@ -138,6 +187,8 @@ class MusicPlayerService : MediaBrowserServiceCompat(), MusicPlayer.Callback {
         }
 
         override fun onStop() {
+            unregisterReceiver(audioNoisyReceiver)
+
             mediaSession.isActive = false
             musicPlayer.stop()
             setNewState(PlaybackStateCompat.STATE_STOPPED)
@@ -184,6 +235,16 @@ class MusicPlayerService : MediaBrowserServiceCompat(), MusicPlayer.Callback {
                     )
         }
         return actions
+    }
+
+    private fun canGainAudioFocus(): Boolean {
+        return when (AudioManagerCompat.requestAudioFocus(audioManager, audioFocusRequest)) {
+            AudioManager.AUDIOFOCUS_REQUEST_GRANTED -> true
+            else -> {
+                Timber.d("requestAudioFocus: failure")
+                false
+            }
+        }
     }
 
     // region MusicPlayer.Callback
